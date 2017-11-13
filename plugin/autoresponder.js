abdad58a50a878eb43ca02ecca949ddc68e8a75a
Matthias Luebken <matthias.luebken@gmail.com>
2017-11-13 16:11:03 +0100

    typos and async, await fixes

--- plugin/autoresponder.js
+++ plugin/autoresponder.js
@@ -1,14 +1,14 @@
 module.exports = function (robot) {
-  robot.on('issues.opened', async context => {
+  robot.on('issues.opened', context => {
     return createComment(robot, context, "opening")
   })
-  robot.on('issues.clised', async context => {
+  robot.on('issues.closed', context => {
     return createComment(robot, context, "closing")
   })
 }
 
 
-function createComment(context, action) {
+const createComment= async (robot, context, action) => {
   const github = await robot.app.asInstallation(context.payload.installation.id)
   const body = { body: `Thanks for ${action} this issue! :tada: :thumbsup: :thumbsup:` }
   return github.issues.createComment(context.issue(body))
