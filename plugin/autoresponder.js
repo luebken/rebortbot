28a62a9e27139a8984b4c468de73dbc67da00793
Matthias Luebken <matthias.luebken@gmail.com>
2017-11-13 15:42:47 +0100

    adding thumbsup

--- plugin/autoresponder.js
+++ plugin/autoresponder.js
@@ -1,6 +1,6 @@
 module.exports = function(robot) {
   robot.on('issues.opened', async context => {
     const github = await robot.app.asInstallation(context.payload.installation.id)
-    return github.issues.createComment(context.issue({ body: 'Thanks for opening this issue! :tada:' }))
+    return github.issues.createComment(context.issue({ body: 'Thanks for opening this issue! :tada: :thumbsup: :thumbsup:' }))
   })
 }
