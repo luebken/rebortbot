25d5d8eaac8fdac306cad6e5a3e9c2267579f646
Matthias Luebken <matthias.luebken@gmail.com>
2017-11-13 16:01:11 +0100

    opening and closing

--- plugin/autoresponder.js
+++ plugin/autoresponder.js
@@ -1,6 +1,15 @@
-module.exports = function(robot) {
+module.exports = function (robot) {
   robot.on('issues.opened', async context => {
-    const github = await robot.app.asInstallation(context.payload.installation.id)
-    return github.issues.createComment(context.issue({ body: 'Thanks for opening this issue! :tada: :thumbsup: :thumbsup:' }))
+    return createComment(robot, context, "opening")
   })
+  robot.on('issues.clised', async context => {
+    return createComment(robot, context, "closing")
+  })
+}
+
+
+function createComment(context, action) {
+  const github = await robot.app.asInstallation(context.payload.installation.id)
+  const body = { body: `Thanks for ${action} this issue! :tada: :thumbsup: :thumbsup:` }
+  return github.issues.createComment(context.issue(body))
 }
